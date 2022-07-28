0191a4c9bad8ab1f29fdb914ed4013dd2697a816
Badre-Edin <dridibadr@gmail.com>
2022-07-28 17:48:05 +0100

    dependecies updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 // call firebase
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 void main() async {
   // call firebase
   WidgetsFlutterBinding.ensureInitialized();
