7ddd4887c464cd836447c5940685f51f4e90c6a7
omarzouaoui <zouaouiomar80@gmail.com>
2022-07-30 18:38:11 +0100

    working on uploading

--- lib/kampy_create_posts.dart
+++ lib/kampy_create_posts.dart
@@ -1,80 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/services/crud.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:random_string/random_string.dart';
 import 'dart:io';
-// class CreateBlog extends StatefulWidget {
-//   CreateBlog({Key? key}) : super(key: key);
 
-//   @override
-//   State<CreateBlog> createState() => _CreateBlogState();
-// }
-
-
-// class _CreateBlogState extends State<CreateBlog> {
-
-// String desc="";
-
-// // CrudMethods crudMethods= new CrudMethods();
-
-
-
-//  final ImagePicker _picker = ImagePicker();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Row(
-//           mainAxisAlignment:MainAxisAlignment.center,
-//           children: const <Widget>[
-//            Text(
-//             "Kampy",
-//             style: TextStyle(
-//               fontSize: 23,color: Colors.blue,
-//             ),
-//           ),
-//           Text("Posts", style: TextStyle(
-//             fontSize: 23,color:Color.fromARGB(255, 0, 0, 0)
-//           ),)
-//         ]),
-//         backgroundColor: Colors.transparent,
-//         elevation: 0.0,
-//         actions:<Widget>[
-//           Container(
-//             padding: EdgeInsets.symmetric(horizontal: 16),
-//             child: Icon(Icons.file_upload)
-//           )
-//         ],
-//       ),
-//       body: Container(
-//         child: Column(children:<Widget> [
-//           SizedBox(height: 10,),
-//           Container(
-//           margin: EdgeInsets.symmetric(horizontal:16),  
-//           height: 150,
-//           decoration: BoxDecoration(
-//             color: Colors.white,borderRadius:BorderRadius.circular(6)
-//           ),
-//           width: MediaQuery.of(context).size.width,
-//           child: Icon(Icons.add_a_photo,color: Colors.black45,),
-//           ),
-//           SizedBox(height: 8,),
-//           Container(
-//             margin: EdgeInsets.symmetric(horizontal: 16),
-//             child: Column(children:<Widget> [
-//               TextField(
-//                 decoration:InputDecoration(hintText: "description"),
-//                 onChanged: (val){
-//                   desc=val;
-//                 },
-//               )
-//             ]),
-//           )
-//         ],),
-//       ),
-//     );
-//   }
-// }
 class CreateBlog extends StatefulWidget {
   const CreateBlog({Key? key}) : super(key: key);
 
@@ -84,6 +15,10 @@ class CreateBlog extends StatefulWidget {
 
 class _CreateBlogState extends State<CreateBlog> {
   File? _image;
+  String desc="";
+  File ?selectedImage;
+  bool _isLoading= false;
+  CrudMethods crudMethods= new CrudMethods();
 
   final _picker = ImagePicker();
   // Implementing the image picker
@@ -96,20 +31,66 @@ class _CreateBlogState extends State<CreateBlog> {
       });
     }
   }
+  
+  // uploadBlog() async{
+  //   if(selectedImage!= null){
+  //     setState(() {
+  //       _isLoading=true;
+  //     });
+  //     StorageReference firebaseStorageRef=FirebaseStorage.instance
+  //     .ref()
+  //     .child('blogImages')
+  //     .child("${randomAlphaNumeric(9)}.jpg");
+  //     final StorageUploadTask task=firebaseStorageRef.putFile(selectedImage);
+
+  //     var downloadUrl = await(await task.onComplete).ref.getDownLoadURL();
+  //     print("this is url $downloadUrl")
+  //   }else{
+
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Kindacode.com'),
-        ),
-        body: SafeArea(
+        title: Row(
+          mainAxisAlignment:MainAxisAlignment.center,
+          children: const <Widget>[
+           Text(
+            "Kampy",
+            style: TextStyle(
+              fontSize: 23,color: Colors.blue,
+            ),
+          ),
+          Text("Posts", style: TextStyle(
+            fontSize: 23,color:Color.fromARGB(255, 0, 0, 0)
+          ),)
+        ]),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        actions:<Widget>[
+          GestureDetector(
+            onTap: () {
+              // uploadBlog();
+            },
+            child:Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Icon(Icons.file_upload),
+          )
+          )
+        ],
+      ),
+        body: _isLoading? SingleChildScrollView():SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(35),
             child: Column(children: [
+
+              //adding pics
+              
               Center(
                 child: ElevatedButton(
-                  child: const Text('Select An Image'),
+                  child: Icon(Icons.add_a_photo),
                   onPressed: _openImagePicker,
                 ),
               ),
@@ -117,14 +98,31 @@ class _CreateBlogState extends State<CreateBlog> {
               Container(
                 alignment: Alignment.center,
                 width: double.infinity,
-                height: 300,
+                height: 200,
                 color: Colors.grey[300],
                 child: _image != null
                     ? Image.file(_image!, fit: BoxFit.cover)
                     : const Text('Please select an image'),
+              ),  SizedBox(height: 8,),
+
+              //input of description
+
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(children:<Widget> [
+                  TextField(
+                    decoration: InputDecoration(hintText: "description"),
+                    onChanged: ((val) {
+                      desc=val;
+                    }),
+                  ),
+                ]),
               )
+              
             ]),
           ),
+          
+          
         ));
   }
 }
--- lib/services/crud.dart
+++ lib/services/crud.dart
@@ -1,9 +1,12 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-// class CrudMethods{
-//   Future<void> addData(postData) async{
-//     Firestore.instance.collection("posts").add(postData).catchError((e){
-//       print(e);
-//     });
-//   }
-// }
+final databaseReference  = FirebaseFirestore.instance;
+
+class CrudMethods{
+  Future<void> addData(postData) async{
+
+    FirebaseFirestore.instance.collection("posts").add(postData).catchError((e){
+      print(e);
+    });
+  }
+}
