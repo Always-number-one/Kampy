42e23cd182c2768af76b69ad29681ed0382db73d
Moez Hamza <hamza.moez@gmail.com>
2022-08-01 16:09:55 +0100

    setting up the chat box

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'kampy_user.dart';
 import 'kampy_event.dart';
 import 'kampy_chat.dart';
 import 'kampy_posts.dart';
 import 'kampy_home.dart';
-
-
+import 'kampy_login.dart';
 
 // call firebase
 import 'package:firebase_core/firebase_core.dart';
@@ -16,7 +14,7 @@ void main() async {
   // call firebase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  // 
+  //
 
   runApp(const MyApp());
 }
@@ -24,15 +22,15 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-    
         primarySwatch: Colors.pink,
       ),
-      
-
+      home: Chat(),
+    );
+  }
+}
