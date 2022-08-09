a2584c237c5bf5e0f32fdda5c56feb08e2f3c1f3
sameh-dh <samehdh44@gmail.com>
2022-08-09 09:34:29 +0100

    psots

--- lib/kampy_posts.dart
+++ lib/kampy_posts.dart
@@ -10,7 +10,7 @@ import './services/crud_posts.dart';
 //import create blogs
 import 'kampy_create_posts.dart';
 
-// hex color 
+// hex color
 import 'package:hexcolor/hexcolor.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -33,208 +33,192 @@ class Posts extends StatefulWidget {
 }
 
 class _PostsState extends State<Posts> {
-  
-
- // navbar
+  // navbar
   final List<Widget> _pages = [KampyEvent(), Posts(), Welcome(), CreatePost()];
 // plus button array of pages
   final List<Widget> _views = [KampyEvent(), Posts(), Chat(), Welcome()];
   int index = 0;
-  
-   postsList() {
-       //  create firestore instance
+
+  postsList() {
+    //  create firestore instance
     FirebaseFirestore firestore = FirebaseFirestore.instance;
-   // grab the collection
-    CollectionReference posts= firestore.collection('posts');
-   return StreamBuilder<QuerySnapshot>(
-              // build dnapshot using users collection
-                stream: posts.snapshots(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasError) {
-                    return const Text("Something went wrong");
-                  }
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Text("loading");
-                  }if (snapshot.hasData){
-
-        return  SingleChildScrollView(
-          
-              padding: const EdgeInsets.only(top: 70),
-            
-                  child: Column(   
-           children:
-           
-           [ 
-             const  Text(
-                          "KAMPY",
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontFamily: 'MuseoModerno',
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                          const SizedBox( height:50,),
-        for (int i=0 ; i<snapshot.data!.docs.length ; i++) 
-Column(
-  
-       children: [ 
-                    
-
-     Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Row(
+    // grab the collection
+    CollectionReference posts = firestore.collection('posts');
+    return StreamBuilder<QuerySnapshot>(
+        // build dnapshot using users collection
+        stream: posts.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Something went wrong");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("loading");
+          }
+          if (snapshot.hasData) {
+            return SingleChildScrollView(
+                padding: const EdgeInsets.only(top: 70),
+                child: Column(children: [
+                  const Text(
+                    "KAMPY",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontFamily: 'MuseoModerno',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  for (int i = 0; i < snapshot.data!.docs.length; i++)
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
-                                CircleAvatar(
-                                        radius: 24.0,
-                                         backgroundImage:  NetworkImage(snapshot.data!.docs[i]['imgUrl']),
-                                      
-                                        backgroundColor: Colors.transparent,
+                                Row(
+                                  children: <Widget>[
+                                    // user avatar
+                                    CircleAvatar(
+                                      radius: 24.0,
+                                      backgroundImage: NetworkImage(
+                                          snapshot.data!.docs[i]['imgUrl']),
+                                      backgroundColor: Colors.transparent,
+                                    ),
+                                    // user name :
+                                    Container(
+                                      padding: const EdgeInsets.only(
+                                          bottom: 5, left: 10),
+                                      child: const Text(
+                                        "sameh",
                                       ),
-                                Container(
-                                  padding: const EdgeInsets.only(bottom: 5, left: 10),
-                                  child:const  Text(
-                                 "sameh",
-                                 
-                                  ),
+                                    ),
+                                    // plus button to delete and update
+                                    Container(
+                                      margin: const EdgeInsets.only(left: 240),
+                                      child: const Icon(
+                                          Icons.control_point_rounded),
+                                    ),
+                                  ],
                                 ),
-                                // plus button to delete and update
-                                  Container(
-                              margin: const EdgeInsets.only(left: 240),
-                         child: const Icon(
-             
-                          Icons.control_point_rounded
-                        ),),
-                              ],
-                            ),
-                          
-                           const  SizedBox(height: 10, ),
-                          //  post image 
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(5),
-                              child: Container(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.4,
-                                child: GestureDetector(
-                                        child: Column(
-                                              children: [
-                                                Container(
-                                                  height: 300,
-                                          width:
-                                              MediaQuery.of(context).size.width,
-                                                decoration:  BoxDecoration(
-                                         image:  DecorationImage(
-                                          image: NetworkImage(
-                                            snapshot.data!.docs[i]['imgUrl'],
-                                           
-                                          
-                                          ),
-                                          fit: BoxFit.fill,
-                                            )
-                             ),
-                             //change photo arrows :
-                        child:  Row(
-  mainAxisAlignment: MainAxisAlignment.spaceAround,
-  children:  <Widget>[
-   IconButton(
-    icon: const Icon( Icons.arrow_back_ios_new_outlined ),
-      color: const  Color.fromARGB(138, 255, 255, 255),
-     iconSize: 36.0,
-    //  next photo
-      onPressed: (){
-                     },
 
-    ),
-  const   SizedBox(width: 265),
-    IconButton(
-     icon: const Icon( Icons.arrow_forward_ios_rounded),
-      color: const Color.fromARGB(138, 255, 255, 255),
-      iconSize: 36.0,
-      // previous photo
-       onPressed: (){
-                     },
-    ),
-  ],
-)
-
-
-                                 ) ,
-                             
-      
-                                       ] ),
-                                       
-                                            ),
-                                          )
-                                      
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                //  post image
+                                ClipRRect(
+                                    borderRadius: BorderRadius.circular(5),
+                                    child: Container(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.4,
+                                      child: GestureDetector(
+                                        child: Column(children: [
+                                          Container(
+                                              height: 300,
+                                              width: MediaQuery.of(context)
+                                                  .size
+                                                  .width,
+                                              decoration: BoxDecoration(
+                                                  image: DecorationImage(
+                                                image: NetworkImage(
+                                                  snapshot.data!.docs[i]
+                                                      ['imgUrl'],
+                                                ),
+                                                fit: BoxFit.fill,
+                                              )),
+                                              //change photo arrows :
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment
+                                                        .spaceAround,
+                                                children: <Widget>[
+                                                  IconButton(
+                                                    icon: const Icon(Icons
+                                                        .arrow_back_ios_new_outlined),
+                                                    color: const Color.fromARGB(
+                                                        138, 255, 255, 255),
+                                                    iconSize: 36.0,
+                                                    //  next photo
+                                                    onPressed: () {},
+                                                  ),
+                                                  const SizedBox(width: 265),
+                                                  IconButton(
+                                                    icon: const Icon(Icons
+                                                        .arrow_forward_ios_rounded),
+                                                    color: const Color.fromARGB(
+                                                        138, 255, 255, 255),
+                                                    iconSize: 36.0,
+                                                    // previous photo
+                                                    onPressed: () {},
+                                                  ),
+                                                ],
+                                              )),
+                                        ]),
                                       ),
-             
-                         const SizedBox(
-                              height: 20,
-                            ),
-                           const SizedBox(
-                              height: 10,),
+                                    )),
 
-                        ] ),),
-                       
-                
-                ],)
-                
-        
-
-
-            ]));
-
-                } 
-                  return const Text("none");
-
-                } 
-             );}
-
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                              ]),
+                        ),
+                      ],
+                    )
+                ]));
+          }
+          return const Text("none");
+        });
+  }
 
   @override
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     
-      body: postsList() ,
+      body: postsList(),
       // navbar bottom
-        bottomNavigationBar:  Builder(builder: (context) =>
-        AnimatedBottomBar(
-            defaultIconColor: Colors.black,
-            activatedIconColor: const Color.fromARGB(255, 56, 3, 33),
-            background: Colors.white,
-            buttonsIcons:const  [Icons.sunny_snowing, Icons.explore_sharp, Icons.messenger_outlined, Icons.person],
-            buttonsHiddenIcons:const  [Icons.campaign_rounded, Icons.shopping_bag, Icons.image_rounded ,Icons.post_add_rounded],
-            backgroundColorMiddleIcon: const Color.fromARGB(255, 56, 3, 33),
-            onTapButton: (i){
-              setState(() {
-                index = i;
-              });
-              Navigator.push(
-              context,
-         MaterialPageRoute(builder: (context) => _views[i]),
-                );
-            },
-            // navigate between pages
-            onTapButtonHidden: (i){
-        
-               Navigator.push(
-              context,
-         MaterialPageRoute(builder: (context) => _pages[i]),
-                );
-            },
-          )
-        ),
+      bottomNavigationBar: Builder(
+          builder: (context) => AnimatedBottomBar(
+                defaultIconColor: Colors.black,
+                activatedIconColor: const Color.fromARGB(255, 56, 3, 33),
+                background: Colors.white,
+                buttonsIcons: const [
+                  Icons.sunny_snowing,
+                  Icons.explore_sharp,
+                  Icons.messenger_outlined,
+                  Icons.person
+                ],
+                buttonsHiddenIcons: const [
+                  Icons.campaign_rounded,
+                  Icons.shopping_bag,
+                  Icons.image_rounded,
+                  Icons.post_add_rounded
+                ],
+                backgroundColorMiddleIcon: const Color.fromARGB(255, 56, 3, 33),
+                onTapButton: (i) {
+                  setState(() {
+                    index = i;
+                  });
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => _views[i]),
+                  );
+                },
+                // navigate between pages
+                onTapButtonHidden: (i) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => _pages[i]),
+                  );
+                },
+              )),
 // navbar bottom ends here
 
-
       backgroundColor: const Color.fromARGB(240, 255, 255, 255),
-  
     );
   }
 }
@@ -274,55 +258,51 @@ class PostsTitle extends StatelessWidget {
             ),
           ),
           Container(
-            
             height: 170,
-          
-         
             decoration: BoxDecoration(
-                color: Colors.black45.withOpacity(0.3),
-                borderRadius: BorderRadius.circular(8),
-                ),
+              color: Colors.black45.withOpacity(0.3),
+              borderRadius: BorderRadius.circular(8),
+            ),
           ),
           Container(
-            margin: const EdgeInsets.fromLTRB(150, 20, 00, 140),
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  localisation,
-                  style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                      color: Color.fromARGB(255, 0, 0, 0)),
-                      
-                ),])),
-                Container(
+              margin: const EdgeInsets.fromLTRB(150, 20, 00, 140),
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      localisation,
+                      style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                  ])),
+          Container(
             margin: EdgeInsets.fromLTRB(190, 30, 00, 10),
             width: MediaQuery.of(context).size.width,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children:<Widget> [
-                Text(description,
-                  style: const TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                ),
-
-              ]),
-                ),
-              ],
-            ),
-          );
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    description,
+                    style: const TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ]),
+          ),
+        ],
+      ),
+    );
   }
 }
-
 
 // class Posts extends StatefulWidget {
 //   Posts({Key? key}) : super(key: key);
@@ -401,7 +381,7 @@ class PostsTitle extends StatelessWidget {
 //             icon: Icon(Icons.account_circle),
 //             label: 'Profile',
 //             backgroundColor: Color.fromARGB(255, 79, 36, 90),
-            
+
 //           ),
 //         ],
 //       ),
